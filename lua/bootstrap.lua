c282de3b52adb8a0779b4dd0d81bdbb156ff250d
Andreas Pohl <pohl@adtech.de>
2016-01-14 14:00:47 +0100

    Lua code cleanup

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -1,13 +1,11 @@
--- Setup the module path
-package.path = package.path
-   .. ";/home/osar/host/workspace/petrel/lua-modules/thrift/?.lua"
-   .. ";/home/osar/host/workspace/petrel/lua-modules/uts/?.lua"
---Require "utsreadinterface_AudiencePerUTIDService"
-
 -- This function is called at boot up time before accepting requests. You need to setup
--- your routes here and you can load cache libraries or configurations that need to be
+-- your routes here and you can load native libraries or set configurations that need to be
 -- ready for request processing.
 function bootstrap()
+   -- Load native libraries
+   --petrel.add_lib_search_path("/non/standard/path/to/your/lib")
+   --petrel.load_lib("petrel_mylib")
+
    -- Setup request handlers
-   petrel.set_route("/advis", "handle_advis")
+   petrel.set_route("/", "handle_request")
 end
--- lua/handle_request.lua
+++ lua/handle_request.lua
@@ -1,44 +1,19 @@
 -- Request handling entry point
 function handle_request(request, response)
    -- print out req table
-   --for k, v in pairs(request) do
-   --   if type(v) == "table" then
-   --      for k1, v1 in pairs(v) do
-   --         print(LOG_DEBUG, "request." .. k .. "[\"" .. k1 .. "\"] = " .. v1)            
-   --      end
-   --   else
-   --      print(LOG_DEBUG, "request." .. k .. " = " .. v)
-   --   end
-   --end
+   print(LOG_DEBUG, request)
 
-   -- test the http_client lib
-   --try(function()
-   --      h = http_client()
-   --      h:connect("dubidam.de")
-   --      status, data = h:get("/")
-   --      print(LOG_INFO, "status: ", status)
-   --      print(LOG_INFO, "response: ", data)
-   --   end, function(e)
-   --      print(LOG_ERR, e)
-   --end)
-   
-   --try(function()
-   --      timer = metric()
-   --      timer:register_metric("test_timer", "timer")
-   --      timer:start_duration()
-   --      counter = metric()
-   --      counter:register_metric("test_counter", "counter")
-   --      counter:increment()
-   --      timer:finish_duration()
-   --    end, function(e)
-   --      print(LOG_ERR, e)
-   --end)
-  
-   petrel.sleep_millis(20)
-   
-   --response.status = 200
-   response.content = "handle_request\n"
-   response.headers["x-myheader"] = "myvalue"
+   counter = metric()
+   counter:register_counter("calls") -- new counter or load existsing
+   counter:increment()
+
+   response.headers["content-type"] = "text/html; charset=UTF-8"
+   response.content =
+      "<html>" ..
+      "<head><title>petrel test page</title></head>" ..
+      "<body><h1>petrel test page</h1>We got called " .. counter:total() .. "</body>" ..
+      "</html>\n"
+
    return response
 end
 

2cd6d62eefc1b49560e50e0aad6923981bd86ffd
Andreas Pohl <pohl@adtech.de>
2016-01-30 14:45:43 +0100

    http_client: add POST support and connect/read timeouts

--- src/lib/builtin/http_client.cpp
+++ src/lib/builtin/http_client.cpp
@@ -12,11 +12,14 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <cstring>
 #include <cstdlib>
 
 #include <boost/utility/string_ref.hpp>
 #include <boost/fiber/all.hpp>
+#include <boost/bind.hpp>
+#include <boost/algorithm/string.hpp>
 #include <petrel/fiber/yield.hpp>
 
 namespace petrel {
@@ -26,11 +29,21 @@ DECLARE_LIB_BEGIN(http_client);
 ADD_LIB_METHOD(connect);
 ADD_LIB_METHOD(disconnect);
 ADD_LIB_METHOD(get);
+ADD_LIB_METHOD(post);
+ADD_LIB_METHOD(read_timeout);
+ADD_LIB_METHOD(connect_timeout);
+ADD_LIB_METHOD(disable_keepalive);
 DECLARE_LIB_BUILTIN_END();
 
-namespace bs = boost::system;
 namespace bf = boost::fibers;
 namespace bfa = bf::asio;
+namespace balg = boost::algorithm;
+
+void http_client::timeout_handler(const bs::error_code& ec) {
+    if (ba::error::operation_aborted != ec) {
+        m_sock.cancel();
+    }
+}
 
 int http_client::connect(lua_State* L) {
     m_host = luaL_checkstring(L, 1);
@@ -44,89 +57,105 @@ int http_client::connect(lua_State* L) {
     try {
         auto& resolver = context().server().get_resolver_cache();
         auto ep_iter = resolver.async_resolve<resolver_cache::tcp>(io_service(), m_host, m_port, bfa::yield);
+        ba::deadline_timer timer(io_service());
+        timer.expires_from_now(m_connect_timeout);
+        timer.async_wait(boost::bind(&http_client::timeout_handler, this, ba::placeholders::error));
         ba::async_connect(m_sock, ep_iter, bfa::yield);
+        timer.cancel();
         m_connected = true;
     } catch (bs::system_error& e) {
         luaL_error(L, "connect failed: %s", e.what());
     }
-    return 0; // no results
+    return 0;  // no results
 }
 
 int http_client::disconnect(lua_State*) {
     close();
-    return 0; // no results
+    return 0;  // no results
 }
 
 int http_client::get(lua_State* L) {
     boost::string_ref path(luaL_checkstring(L, 1));
-
     if (!m_connected) {
         luaL_error(L, "not connected");
     }
-
     // Create request
     std::ostream request_strm(&m_req_buf);
     request_strm << "GET " << path << " HTTP/1.1\r\n";
     request_strm << "Host: " << m_host << "\r\n";
     request_strm << "Accept: */*\r\n";
     request_strm << "User-Agent: petrel/http_client\r\n";
-    request_strm << "Connection: close\r\n\r\n";
+    if (!m_keep_alive) {
+        request_strm << "Connection: keep-alive\r\n";
+    }
+    request_strm << "\r\n";
+    return send_recv(L);
+}
 
+int http_client::post(lua_State* L) {
+    boost::string_ref path(luaL_checkstring(L, 1));
+    const char* data = nullptr;
+    std::size_t len = 0;
+    if (lua_isstring(L, 2)) {
+        data = lua_tolstring(L, 2, &len);
+    }
+    if (!m_connected) {
+        luaL_error(L, "not connected");
+    }
+    // Create request
+    std::ostream request_strm(&m_req_buf);
+    request_strm << "POST " << path << " HTTP/1.1\r\n";
+    request_strm << "Host: " << m_host << "\r\n";
+    request_strm << "Accept: */*\r\n";
+    request_strm << "User-Agent: petrel/http_client\r\n";
+    if (!m_keep_alive) {
+        request_strm << "Connection: keep-alive\r\n";
+    }
+    request_strm << "Content-Length: " << len << "\r\n\r\n";
+    if (len > 0 && nullptr != data) {
+        request_strm.write(data, len);
+    }
+    return send_recv(L);
+}
+
+int http_client::send_recv(lua_State* L) {
     try {
         // send query
         ba::async_write(m_sock, m_req_buf, bfa::yield);
+        ba::deadline_timer timer(io_service());
+        timer.expires_from_now(m_read_timeout);
+        timer.async_wait(boost::bind(&http_client::timeout_handler, this, ba::placeholders::error));
         bs::error_code ec;
-        std::size_t n = ba::async_read(m_sock, ba::buffer(m_res_buf, sizeof(m_res_buf) - 1), bfa::yield[ec]);
+        ba::async_read_until(m_sock, m_res_buf, "\r\n\r\n", bfa::yield[ec]);
+        timer.cancel();
         if (ec && ec != ba::error::eof) {
             luaL_error(L, "read failed: %s", ec.message().c_str());
         }
-        m_res_buf[n] = 0;
+        std::istream response_strm(&m_res_buf);
+        std::string line;
         // read status line
-        if (std::strncmp(m_res_buf, "HTTP/", 5)) {
+        std::getline(response_strm, line);
+        if (!response_strm || line.find("HTTP/") != 0) {
             luaL_error(L, "invalid response");
         }
-        const char* pos = std::strchr(m_res_buf + 5, ' ');
-        if (nullptr == pos) {
-            close();
+        auto pos = line.find(' ', 5);
+        if (std::string::npos == pos) {
             luaL_error(L, "invalid response: no status");
         }
-        int status = std::atoi(pos + 1);
+        int status = std::atoi(line.c_str() + pos + 1);
         if (status < 100 || status > 999) {
             luaL_error(L, "invalid response: invalid status %d");
         }
-        pos = std::strchr(pos + 4, '\n');
-        if (nullptr == pos) {
-            luaL_error(L, "invalid response: newline expected");
-        }
-        pos++;
         // read headers
         std::string location;
-        while (*pos != 0) {
-            // end of headers check
-            if (*pos == '\r' || *pos == '\n') {
-                pos++;
-                if (*pos == '\n') {
-                    pos++;
-                }
-                break;
+        std::size_t content_len = 0;
+        while (std::getline(response_strm, line) && line != "\r") {
+            if (balg::istarts_with(line, "location: ")) {
+                location = line.substr(10);
+            } else if (balg::istarts_with(line, "content-length: ")) {
+                content_len = std::atoi(line.c_str() + 16);
             }
-            // look for the next line break
-            const char* eol = std::strchr(pos, '\n');
-            if (nullptr == eol) {
-                luaL_error(L, "invalid header: newline expected");
-            }
-            // look for the location header
-            if (status == 302 && location.empty() && !strncmp(pos, "Location: ", 10)) {
-                const char* loc_start = pos + 10;
-                char* loc_end = const_cast<char*>(eol);
-                while (*(loc_end - 1) == '\r' || *(loc_end - 1) == '\n') {
-                    loc_end--;
-                }
-                *loc_end = 0;
-                location = loc_start;
-            }
-            pos = eol + 1;
-        };
+        }
         // check for redirect
         if (status == 302) {
             if (location.empty()) {
@@ -136,12 +165,48 @@ int http_client::get(lua_State* L) {
             lua_pushstring(L, location.c_str());
         } else {
             lua_pushinteger(L, status);
-            lua_pushstring(L, pos);
+            if (content_len > 0) {
+                auto to_read = content_len - m_res_buf.size();
+                if (to_read > 0) {
+                    // if the buffer does not contain the full content yet, receive the rest
+                    timer.expires_from_now(m_read_timeout);
+                    timer.async_wait(boost::bind(&http_client::timeout_handler, this, ba::placeholders::error));
+                    ba::async_read(m_sock, m_res_buf, boost::asio::transfer_at_least(to_read), bfa::yield[ec]);
+                    timer.cancel();
+                    if (ec && ec != ba::error::eof) {
+                        luaL_error(L, "read failed: %s", ec.message().c_str());
+                    }
+                }
+                luaL_Buffer lbuf;
+                luaL_buffinitsize(L, &lbuf, content_len);
+                std::for_each(ba::buffers_begin(m_res_buf.data()), ba::buffers_end(m_res_buf.data()),
+                              [&lbuf](const char c) { luaL_addchar(&lbuf, c); });
+                luaL_pushresult(&lbuf);
+            } else {
+                lua_pushstring(L, "");
+            }
         }
     } catch (bs::system_error& e) {
         luaL_error(L, e.what());
     }
-    return 2; // status code and data makes 2 results
+    return 2;  // status code and data makes 2 results
+}
+
+int http_client::read_timeout(lua_State* L) {
+    int t = luaL_checkinteger(L, 1);
+    m_read_timeout = bp::millisec(t);
+    return 0;
+}
+
+int http_client::connect_timeout(lua_State* L) {
+    int t = luaL_checkinteger(L, 1);
+    m_connect_timeout = bp::millisec(t);
+    return 0;
+}
+
+int http_client::disable_keepalive(lua_State* L) {
+    m_keep_alive = false;
+    return 0;
 }
 
 }  // lib
--- src/lib/builtin/http_client.h
+++ src/lib/builtin/http_client.h
@@ -17,22 +17,37 @@ namespace petrel {
 namespace lib {
 
 namespace ba = boost::asio;
+namespace bp = boost::posix_time;
+namespace bs = boost::system;
+
 
 class http_client : public library {
   public:
-    http_client(lib_context* ctx) : library(ctx), m_sock(m_iosvc) {}
+    http_client(lib_context* ctx)
+        : library(ctx), m_sock(m_iosvc), m_read_timeout(0, 0, 5, 0), m_connect_timeout(0, 0, 5, 0) {}
 
     int connect(lua_State* L);
     int disconnect(lua_State* L);
     int get(lua_State* L);
+    int post(lua_State* L);
+    int read_timeout(lua_State* L);
+    int connect_timeout(lua_State* L);
+    int disable_keepalive(lua_State* L);
 
   private:
     ba::ip::tcp::socket m_sock;
     ba::streambuf m_req_buf;
-    char m_res_buf[8192];
+    ba::streambuf m_res_buf;
     std::string m_host;
     std::string m_port;
+    bp::time_duration m_read_timeout;
+    bp::time_duration m_connect_timeout;
+    bool m_keep_alive = true;
     bool m_connected = false;
+
+    int send_recv(lua_State* L);
+
+    void timeout_handler(const bs::error_code& ec);
 
     inline void close() {
         if (m_connected) {
